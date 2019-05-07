b0c2575f7a629365799a450bc5471be67512367f
ColinVenida <colin.venida@gmail.com>
2019-05-07 05:06:57 -0700

    added testscene
    added testscene to see what apects of an image I can edit through script

---
+++ Bear Stories/Assets/_Scripts/ColorChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Image testImage;
+
+    public void ColorChange( int color )
+    {
+        switch( color) 
+        {
+            case 0: // white/default
+                testImage.color = new Color( 1, 1, 1 );
+                break;
+            case 1: //red
+                testImage.color = new Color( 1, 0, 0 );
+                break;
+            case 2: //green
+                testImage.color = new Color( 0, 1, 0 );
+                break;
+            case 3: //blue
+                testImage.color = new Color( 0, 0, 1 );
+                break;
+            default:
+                break;
+        }//end switch
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
