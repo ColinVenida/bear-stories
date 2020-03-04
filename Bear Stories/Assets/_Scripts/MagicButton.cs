5a262b280055240e0fccf28ac7deceefece072fa
ColinVenida <colin.venida@gmail.com>
2020-03-03 23:34:34 -0800

    Animation working as intended
    I studied a youtube tutorial on the Unity Animator and got the elephant picture to animate on when player presses the MagicButton.  The animation stops when the player presses the button a second time

--- Bear Stories/Assets/_Scripts/MagicButton.cs
+++ Bear Stories/Assets/_Scripts/MagicButton.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MagicButton : MonoBehaviour
 {
-
-    public List<Animation> animList;
+    public Animator animator;  
     public List<ImageSwaper> swapList;
     public List<AudioClip> audioList;
 
     private AudioSource source;
+    private bool animate = false;
 
     public void MakeMagic()
     {
-        //play the stuff here
-        Debug.Log( "Magic here" );
-    }
+        //play the stuff here        
 
+        animate = !animate;
+        //Debug.Log( "animate = " + animate );
+        if ( animate )
+        {
+            //Debug.Log("Magic Start");
+            animator.SetBool("PlayAnimation", animate);           
+        }
+        else
+        {
+            //Debug.Log("Magic Stop");
+            animator.SetBool("PlayAnimation", animate);
+        }
+    }
+    
     //audio tutorial here
     //https://unity3d.com/learn/tutorials/topics/audio/sound-effects-scripting
     void Start()
-    {
-        animList = new List<Animation>();
+    {        
         swapList = new List<ImageSwaper>();
         audioList = new List<AudioClip>();
     }
