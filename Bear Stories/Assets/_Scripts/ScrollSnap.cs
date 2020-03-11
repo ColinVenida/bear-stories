06a4c0b8b04fbba74f71d86e65c413a62b07812f
ColinVenida <colin.venida@gmail.com>
2020-03-10 20:14:34 -0700

    Created a class that automatically adjusts a dropdown's viewport to the selected item

---
+++ Bear Stories/Assets/_Scripts/ScrollSnap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//behavior that automatically adjusts a dropdown's viewport to show the selected item
+
+public class ScrollSnap : MonoBehaviour
+{
+   
+    
+    //TODO:  is there a way to get these references without setting them to public, and then drag-dropping them in the editor?
+
+    public Dropdown parentDrop;             //referenct to the parent's dropdown component
+    public RectTransform contentTrans;      //reference to the Content object
+                                            //content.position.y controls the location of the viewport and scrollbar 
+   
+    private void OnEnable()
+    {
+        //***Alert***
+        //OnEnable() is called multiple times when the dropdown is clicked.  I'm guessing the dropdown's template OnEnable() is called, and the
+            //OnEnable() of the actual DropdownList object is called
+
+        //dividing (parentDrop.value + 1) / (total items in the Dropdown) shows us how far down the viewport we need to go
+        float y = contentTrans.rect.height * (parentDrop.value + 1 / parentDrop.options.Count);        
+        contentTrans.position = new Vector3 ( 0 , y);      
+    }
+
+  
+}
---
+++ Bear Stories/Assets/_Scripts/TestEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class to test animation events
+public class TestEvent : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private bool animate = false;
+
+    public void PrintAwesome()
+    {
+        Debug.Log( "Awesome!" );
+        animator.SetBool("animate", false);
+    }
+
+    public void Magic()
+    {
+        animate = !animate;
+        animator.SetBool("animate", true);
+    }
+
+}
