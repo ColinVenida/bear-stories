6048f52d4bbe56352f11d87e22c8af5e02185178
ColinVenida <colin.venida@gmail.com>
2020-04-02 10:35:58 -0700

    Architecture Refactor
    Saving progress on adding mulitple pages to the project.  About to start a new branch

---
+++ Bear Stories/Assets/_Scripts/PageTurner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageTurner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prevPage;
+    public GameObject currentPage;
+    public GameObject nextPage;
+
+    public void NextPage()
+    {
+        if ( nextPage != null )
+        {           
+            nextPage.gameObject.SetActive( true );
+            currentPage.gameObject.SetActive( false );
+        }        
+    }
+
+    public void PrevPage()
+    {
+        if ( prevPage != null)
+        {
+            prevPage.gameObject.SetActive( true );
+            currentPage.gameObject.SetActive( false );
+        }            
+    }
+
+}
