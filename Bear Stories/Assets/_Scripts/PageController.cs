cdefd9db5ee80a53522a55a9a199707210a34374
ColinVenida <colin.venida@gmail.com>
2020-04-02 11:47:32 -0700

    Added a system to turn pages in the book
    *Note* the page turning system does not work with 2-page books.  Books need to be 3 or more pages

---
+++ Bear Stories/Assets/_Scripts/PageController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PageController : MonoBehaviour
+{
+
+    public GameObject[] PageArray;
+    public Button btnNext;
+    public Button btnPrev;
+
+    private int currentPage;
+
+    public void NextPage()
+    {
+        //check array bounds here
+        if ( currentPage + 1 < PageArray.Length )
+        {            
+            TurnPage( currentPage + 1 );
+        }
+    }
+
+    public void PrevPage()
+    {
+        if ( currentPage - 1 >= 0 )
+        {
+            TurnPage( currentPage - 1 );
+        }
+    }
+
+    private void TurnPage( int page )
+    {
+        //Debug.Log( "PageArray.Length == " + PageArray.Length );
+        //Debug.Log( "currentPage == " + currentPage );
+        //Debug.Log( "page == " + page );
+
+        //deactivate the current page, and activate the next page
+        PageArray[currentPage].gameObject.SetActive( false );       
+        PageArray[page].gameObject.SetActive( true );
+  
+        //update the currentPage reference
+        currentPage = page;
+
+        CheckPageBounds();
+    }
+
+    private void CheckPageBounds()
+    {
+        //check whether we are at the beginning or end of the book, then turn off the appropriate next/prev button
+
+        //Debug.Log( "CheckPageBounds() currentPage == " + currentPage );
+        //**NOTE** this logic does not work with 2-page books!  Books must have 3 or more pages
+        if (currentPage == 0)
+        {
+            btnPrev.gameObject.SetActive( false );           
+        }
+        else if (currentPage == PageArray.Length - 1)
+        {
+            btnNext.gameObject.SetActive( false );            
+        }
+        else
+        {
+            btnNext.gameObject.SetActive( true );
+            btnPrev.gameObject.SetActive( true );
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPage = 0;
+        CheckPageBounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
