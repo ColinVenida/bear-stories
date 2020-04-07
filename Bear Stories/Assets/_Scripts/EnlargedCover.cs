aad686f9358955c96b3d2ae64b32944bec895de0
ColinVenida <colin.venida@gmail.com>
2020-04-07 07:14:39 -0700

    Added a main menu so users can select what book they want to read
    Added BookCover prefab and EnlargedImage Object and Class
    -BookCovers activate the EnlargedImage object in the scene and sets the image's sprite to the BookCover's sprite
    -BookCover has an int that acts as a bookId.  bookId is copied to the EnlargedImage
    
    When the "Read" buton is pressed on the EnlargedImage, the program switches the scene based on the bookId

---
+++ Bear Stories/Assets/_Scripts/EnlargedCover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnlargedCover : MonoBehaviour
+{
+    public Image cover;
+    public Button closeBtn;
+    public Button readBtn;
+    
+    private int bookId;
+
+    public void ReadBook()
+    {
+        //switch the scene based on the bookId
+        switch( bookId )
+        {
+            case 0:
+                SceneManager.LoadScene( "TestScene" );
+                break;
+            //case 1:
+            //    break;
+            //case 2:
+            //    break;           
+            default:
+                Debug.Log( "bookId not set.  bookId = " + bookId );
+                break;
+        }
+    }
+
+    public void CloseCover()
+    {
+        this.gameObject.SetActive( false );
+    }
+
+    public void SetBookId( int id )
+    {
+        bookId = id;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Bear Stories/Assets/_Scripts/ImageEnlarger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+//class that enlarges the book cover when it's clicked
+public class ImageEnlarger : MonoBehaviour
+{
+
+    //public Image enlargedCover;
+    public EnlargedCover enlargedCover;
+    public int bookId;
+
+    private Image coverImage;
+    private bool isEnlarged = false;
+
+    UnityEvent enlargeCover = new UnityEvent();
+   
+    public void Enlarge()
+    {        
+        enlargeCover.Invoke();        
+    }
+
+    void ShowCover()
+    {
+        //isEnlarged = !isEnlarged;
+
+        ////activate the enlargedCover and set the sprite
+        //if (isEnlarged)
+        //{
+        //    enlargedCover.cover.gameObject.SetActive( true );
+        //    enlargedCover.cover.sprite = coverImage.sprite;
+        //    enlargedCover.SetBookId( bookId );
+        //}
+        //else
+        //{
+        //    enlargedCover.cover.gameObject.SetActive( false );
+        //    enlargedCover.cover.sprite = null;
+        //}
+               
+        //activate the enlargedCover and set the sprite       
+        enlargedCover.cover.gameObject.SetActive( true );
+        enlargedCover.cover.sprite = coverImage.sprite;
+        enlargedCover.SetBookId( bookId );
+    }
+
+    private void Start()
+    {        
+        enlargeCover.AddListener( ShowCover );
+        coverImage = this.GetComponent<Image>();
+    }
+}
