e1dbe65c6e4f7ac4fd4ec485fbd35baf3f998d71
ColinVenida <colin.venida@gmail.com>
2020-04-07 07:19:31 -0700

    update BookCover prefab and deleted commented code

--- Bear Stories/Assets/_Scripts/ImageEnlarger.cs
+++ Bear Stories/Assets/_Scripts/ImageEnlarger.cs
@@ -6,14 +6,11 @@ using UnityEngine.Events;
 
 //class that enlarges the book cover when it's clicked
 public class ImageEnlarger : MonoBehaviour
-{
-
-    //public Image enlargedCover;
+{   
     public EnlargedCover enlargedCover;
     public int bookId;
 
-    private Image coverImage;
-    private bool isEnlarged = false;
+    private Image coverImage;    
 
     UnityEvent enlargeCover = new UnityEvent();
    
@@ -23,22 +20,7 @@ public class ImageEnlarger : MonoBehaviour
     }
 
     void ShowCover()
-    {
-        //isEnlarged = !isEnlarged;
-
-        ////activate the enlargedCover and set the sprite
-        //if (isEnlarged)
-        //{
-        //    enlargedCover.cover.gameObject.SetActive( true );
-        //    enlargedCover.cover.sprite = coverImage.sprite;
-        //    enlargedCover.SetBookId( bookId );
-        //}
-        //else
-        //{
-        //    enlargedCover.cover.gameObject.SetActive( false );
-        //    enlargedCover.cover.sprite = null;
-        //}
-               
+    {               
         //activate the enlargedCover and set the sprite       
         enlargedCover.cover.gameObject.SetActive( true );
         enlargedCover.cover.sprite = coverImage.sprite;
