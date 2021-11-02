f3d5f290d59c6e5c5695da17891072513d6ea14f
ColinVenida <colin.venida@gmail.com>
2021-11-01 23:46:06 -0700

    Upgrade to Unity 2020.3.21f1

---
+++ Bear Stories/Assets/_Scripts/PolicyLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolicyLink : MonoBehaviour
+{
+   public void OpenUnityPolicy()
+   {
+        Application.OpenURL( "https://unity3d.com/legal/privacy-policy" );
+   }
+}
